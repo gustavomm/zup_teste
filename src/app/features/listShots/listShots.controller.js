9533fcd98e1b0af5c59c94c7cc481b42abc9fdaa
Gustavo Moreira <gustavomm22.gm@gmail.com>
2017-03-26 20:12:25 -0500

    paginacao

--- src/app/features/listShots/listShots.controller.js
+++ src/app/features/listShots/listShots.controller.js
@@ -1,15 +1,29 @@
 export default class ListShotsController {
   constructor(getShots) {
-    this.shots = getShots;
+    this.getShotsService = getShots;
+    this.shots = '';
+    this.page = 0;
   }
 
   showShots() {
+    this.page++;
     let that = this;
-  	this.shots.getJson('').then(function(data){
-        console.log(data.data.data);
+  	this.getShotsService.getJson(this.page).then(function(data){
+      if (that.shots != ''){
+        for (var key in data.data.data) {
+          let tmp = {};
+          let int_key = parseInt(key);
+          int_key = int_key + (12 * (that.page-1));
+          tmp[int_key] = data.data.data[key];
+          that.shots.push(tmp[int_key]);
+        }
+      }
+      else {
         that.shots = data.data.data;
+      }  
+      console.log(that.shots);
     });
   }
 }
 
-ListShotsController.$inject = [ 'getShots'];
+ListShotsController.$inject = [ 'getShots' ];
--- src/app/services/getShots.service.js
+++ src/app/services/getShots.service.js
@@ -3,17 +3,11 @@ import angular from 'angular';
 class GetShots {
   constructor($http) {
     this.$http = $http;
-    this.token = '?access_token=5db2b9398e10f8ef7824a7047382b94b7f6eeb289a8a38d6f551b164cd4a3a3f';
+    this.token = 'access_token=5db2b9398e10f8ef7824a7047382b94b7f6eeb289a8a38d6f551b164cd4a3a3f';
   }
 
-  getJson(shot) {
-    let query = '';
-    if (shot != '') {
-      query = 'https://api.dribbble.com/v1/shots' + '/' + shot + this.token;
-    }
-    else{
-      query = 'https://api.dribbble.com/v1/shots' + this.token;
-    }
+  getJson(page) {
+    let query = 'https://api.dribbble.com/v1/shots' + '?page=' + page + '&' + this.token ;
     return this.$http.jsonp(query);
   }
 }
