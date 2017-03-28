dd6b3d62d6c4b0a0b58c0c0954b52d8e4b537879
Gustavo Moreira <gustavomm22.gm@gmail.com>
2017-03-27 19:49:23 -0500

    mudando tamanho das IMGs

--- src/app/features/listShots/listShots.controller.js
+++ src/app/features/listShots/listShots.controller.js
@@ -1,8 +1,13 @@
 export default class ListShotsController {
-  constructor(getShots) {
+  constructor(getShots, $scope) {
     this.getShotsService = getShots;
     this.shots = '';
     this.page = 0;
+    this.$scope = $scope;
+    this.layout = {
+      col: 'col-xs-6 col-sm-4 col-md-3 col-lg-2',
+      img: 'teaser'
+    };
   }
 
   showShots() {
@@ -25,6 +30,16 @@ export default class ListShotsController {
     });
   }
 
+  smallShots(){
+    this.layout.col = 'col-xs-6 col-sm-4 col-md-3 col-lg-2';
+    this.layout.img = 'teaser';
+  }
+
+  normalShots(){
+    this.layout.col = 'col-xs-12 col-sm-6 col-md-4 col-lg-3';
+    this.layout.img = 'normal';
+  }
+
 }
 
-ListShotsController.$inject = [ 'getShots' ];
+ListShotsController.$inject = [ 'getShots', '$scope' ];
